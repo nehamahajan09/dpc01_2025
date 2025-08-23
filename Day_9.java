46214d327fb9b57a24883d224cdd79a08c9a82b1
Neha Mahajan <145062296+nehamahajan09@users.noreply.github.com>
2025-08-23 23:01:39 +0530

    Create Day_9.java
    Day 9 Challenge

---
+++ Day_9.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Day_9 {
+    public static String longestCommonPrefix(String[] strs) {
+            if (strs == null || strs.length == 0) {
+                return "";
+            }
+            String prefix = strs[0];
+            for (int i = 1; i < strs.length; i++) {
+                while (strs[i].indexOf(prefix) != 0) {
+                    prefix = prefix.substring(0, prefix.length() - 1);
+                    if (prefix.isEmpty()) {
+                        return "";
+                    }
+                }
+            }
+            return prefix;
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter number of strings: ");
+            int n = sc.nextInt();
+            sc.nextLine();
+            String[] strs = new String[n];
+            System.out.println("Enter strings:");
+            for (int i = 0; i < n; i++) {
+                strs[i] = sc.nextLine().toLowerCase();
+            }
+            String result = longestCommonPrefix(strs);
+            System.out.println("Longest Common Prefix: \"" + result + "\"");
+        }
+    }
+
+
