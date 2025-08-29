ef15dca5864434e8d3612d78cf6f1ded8733fc14
Neha Mahajan <145062296+nehamahajan09@users.noreply.github.com>
2025-08-29 22:07:52 +0530

    Create Day_15.java
    Day 15 Challenge

---
+++ Day_15.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Day_15 {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        int result = lengthOfLongestSubstring(input);
+        System.out.println("Length of longest substring without repeating characters: " + result);
+        
+        sc.close();
+    }
+}
